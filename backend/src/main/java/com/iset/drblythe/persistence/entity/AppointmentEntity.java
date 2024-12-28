2604e8f5a1f0bbc2347590152a5d572f2d397a69
asma-bouzidi <b.asma20042004@gmail.com>
2024-12-28 19:39:57 +0100

    add one-to-many relation between patient and appointment

--- backend/src/main/java/com/iset/drblythe/persistence/entity/AppointmentEntity.java
+++ backend/src/main/java/com/iset/drblythe/persistence/entity/AppointmentEntity.java
@@ -1,50 +1,50 @@
 package com.iset.drblythe.persistence.entity;
 
-import java.time.LocalDate;
-import java.util.UUID;
-
-import org.springframework.format.annotation.DateTimeFormat;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Getter
 @Setter
-@Table(name="appointment")
+@Table(name = "appointment")
 public class AppointmentEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
 
-    @Column(name = "status", length = 100)
-    private String status;
+  @Column(name = "status", length = 100)
+  private String status;
 
-    @Column(name = "primaryPhysician", length = 100)
-    private String primaryPhysician;
-    
-    @Column(name = "reason", length = 100)
-    private String reason;
-    
-    @Column(name = "note", length = 100)
-    private String note;
-    
-    @Column(name = "cancellationReason", length = 100)
-    private String cancellationReason;
+  @Column(name = "primaryPhysician", length = 100)
+  private String primaryPhysician;
 
-    @Column(name = "schedule")
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private LocalDate schedule;
+  @Column(name = "reason", length = 100)
+  private String reason;
 
-    /*@Column(name = "userId", length = 100)
-    private String userId;*/
+  @Column(name = "note", length = 100)
+  private String note;
 
-    //patient: Patient;
+  @Column(name = "cancellationReason", length = 100)
+  private String cancellationReason;
+
+  @Column(name = "schedule")
+  @DateTimeFormat(pattern = "yyyy-MM-dd")
+  private LocalDate schedule;
+
+  @ManyToOne
+  @JoinColumn(name = "patient_id")
+  private PatientEntity patient;
+
 }
--- backend/src/main/java/com/iset/drblythe/persistence/entity/PatientEntity.java
+++ backend/src/main/java/com/iset/drblythe/persistence/entity/PatientEntity.java
@@ -1,15 +1,17 @@
 package com.iset.drblythe.persistence.entity;
 
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import java.util.UUID;
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
+import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,74 +21,77 @@ import lombok.Setter;
 @Table(name = "patient")
 public class PatientEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
 
-    @Column(name = "email", length = 100)
-    private String email;
+  @Column(name = "email", length = 100)
+  private String email;
 
-    @Column(name = "phone", length = 100)
-    private String phone;
+  @Column(name = "phone", length = 100)
+  private String phone;
 
-    @Column(name = "name", length = 100)
-    private String name;
+  @Column(name = "name", length = 100)
+  private String name;
 
-    @Column(name = "emergency_contact_name", length = 100)
-    private String emergencyContactName;
+  @Column(name = "emergency_contact_name", length = 100)
+  private String emergencyContactName;
 
-    @Column(name = "emergency_contact_number", length = 100)
-    private String emergencyContactNumber;
+  @Column(name = "emergency_contact_number", length = 100)
+  private String emergencyContactNumber;
 
-    @Column(name = "insurance_provider", length = 100)
-    private String insuranceProvider;
+  @Column(name = "insurance_provider", length = 100)
+  private String insuranceProvider;
 
-    @Column(name = "insurance_policy_number", length = 100)
-    private String insurancePolicyNumber;
+  @Column(name = "insurance_policy_number", length = 100)
+  private String insurancePolicyNumber;
 
-    @Column(name = "allergies", length = 100)
-    private String allergies;
+  @Column(name = "allergies", length = 100)
+  private String allergies;
 
-    @Column(name = "current_medication", length = 100)
-    private String currentMedication;
+  @Column(name = "current_medication", length = 100)
+  private String currentMedication;
 
-    @Column(name = "primary_consent")
-    private Boolean primaryConsent;
+  @Column(name = "primary_consent")
+  private Boolean primaryConsent;
 
-    @Column(name = "gender", length = 100)
-    private String gender;
+  @Column(name = "gender", length = 100)
+  private String gender;
 
-    @Column(name = "age", length = 3)
-    private Integer age;
+  @Column(name = "age", length = 3)
+  private Integer age;
 
-    @Column(name = "address", length = 100)
-    private String address;
+  @Column(name = "address", length = 100)
+  private String address;
 
-    @Column(name = "occupation", length = 100)
-    private String occupation;
+  @Column(name = "occupation", length = 100)
+  private String occupation;
 
-    @Column(name = "family_medical_history", length = 100)
-    private String familyMedicalHistory;
+  @Column(name = "family_medical_history", length = 100)
+  private String familyMedicalHistory;
 
-    @Column(name = "past_medical_history", length = 100)
-    private String pastMedicalHistory;
+  @Column(name = "past_medical_history", length = 100)
+  private String pastMedicalHistory;
 
-    @Column(name = "identification_type", length = 100)
-    private String identificationType;
+  @Column(name = "identification_type", length = 100)
+  private String identificationType;
 
-    @Column(name = "identification_number", length = 100)
-    private String identificationNumber;
+  @Column(name = "identification_number", length = 100)
+  private String identificationNumber;
 
-    @Column(name = "identification_document_id", length = 100)
-    private String identificationDocumentId;
+  @Column(name = "identification_document_id", length = 100)
+  private String identificationDocumentId;
 
-    @Column(name = "identification_document_url", length = 100)
-    private String identificationDocumentUrl;
+  @Column(name = "identification_document_url", length = 100)
+  private String identificationDocumentUrl;
 
-    @Column(name = "primary_physician", length = 100)
-    private String primaryPhysician;
+  @Column(name = "primary_physician", length = 100)
+  private String primaryPhysician;
 
-    @ManyToOne
-    @JoinColumn(name = "doctor_id")
-    private DoctorEntity doctor;
+  @ManyToOne
+  @JoinColumn(name = "doctor_id")
+  private DoctorEntity doctor;
+
+  @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL)
+  private List<AppointmentEntity> appointments;
 }
