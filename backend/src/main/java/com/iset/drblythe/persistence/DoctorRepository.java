b45b25dc1986862c365ed623df9ae2dc4d1d2b0c
Isra Bouzidi <b.isra2004@gmail.com>
2024-12-29 10:27:31 +0100

    Merge pull request #15
    * wip: investigate create doctor with patients
    
    * solve create doctor with patients

--- backend/src/main/java/com/iset/drblythe/persistence/DoctorRepository.java
+++ backend/src/main/java/com/iset/drblythe/persistence/DoctorRepository.java
@@ -1,5 +1,8 @@
 package com.iset.drblythe.persistence;
 
+import com.iset.drblythe.model.Patient;
+import com.iset.drblythe.persistence.mappers.PatientMapper;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -11,6 +14,7 @@ import com.iset.drblythe.persistence.entity.DoctorEntity;
 import com.iset.drblythe.persistence.mappers.DoctorMapper;
 
 import lombok.AllArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @AllArgsConstructor
@@ -19,7 +23,9 @@ public class DoctorRepository {
     public static final String DOCTOR_ID_NOT_FOUND = "Doctor with following Id is not found: ";
 
     private final DoctorMapper doctorMapper;
+    private final PatientMapper patientMapper;
     private final DoctorJpaRepository doctorJpaRepository;
+    private final PatientJpaRepository patientJpaRepository;
 
     public List<Doctor> getAllDoctor(){
   
@@ -40,9 +46,22 @@ public class DoctorRepository {
         return doctorMapper.doctorEntityToDoctor(updatedDoctorEntity);
     }
 
+    @Transactional
      public Doctor createDoctor(Doctor doctor){
         var doctorEntity = doctorMapper.doctorToDoctorEntity(doctor);
-        DoctorEntity newDoctorEntity = doctorJpaRepository.save(doctorEntity);
+
+        doctorEntity.setPatients(new ArrayList<>());
+         if (doctor.getPatients() != null) {
+             for (Patient patient: doctor.getPatients()) {
+                 var patientEntity = patientMapper.patientToPatientEntity(patient);
+                 patientEntity.setDoctor(doctorEntity);
+
+                 var newPatientEntity = patientJpaRepository.save(patientEntity);
+                 doctorEntity.getPatients().add(newPatientEntity);
+             }
+         }
+
+        var newDoctorEntity = doctorJpaRepository.save(doctorEntity);
         return doctorMapper.doctorEntityToDoctor(newDoctorEntity);
     }
 
