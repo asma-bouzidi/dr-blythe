bed6212953c7549fe18cd176b37aae357d3dcd4d
isra-bouzidi <b.isra2004@gmail.com>
2025-01-06 17:16:42 +0100

    DBL-20: refactor and clean template

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,14 +8,14 @@ import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { AppRoutes } from './app.routing';
 import { AppComponent } from './app.component';
 
-import { FullComponent } from './layouts/full/full.component';
-import { AppHeaderComponent } from './layouts/full/header/header.component';
-import { AppSidebarComponent } from './layouts/full/sidebar/sidebar.component';
+import { FullComponent } from './components/layouts/full/full.component';
+import { AppHeaderComponent } from './components/layouts/full/header/header.component';
+import { AppSidebarComponent } from './components/layouts/full/sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemoMaterialModule } from './demo-material-module';
 
-import { SharedModule } from './shared/shared.module';
-import { SpinnerComponent } from './shared/spinner.component';
+import { SharedModule } from './components/shared/shared.module';
+import { SpinnerComponent } from './components/shared/spinner.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +23,7 @@ import { SpinnerComponent } from './shared/spinner.component';
     FullComponent,
     AppHeaderComponent,
     SpinnerComponent,
-    
+
   ],
   imports: [
     BrowserModule,
