5773c76c4aae616b96f4349997d82fdc29123b41
isra-bouzidi <b.isra2004@gmail.com>
2025-01-20 16:50:31 +0100

    backend qdd fields to doctor table

---
+++ backend/src/main/resources/db/migrations/20250120-1_add_fields_to_doctor_table.sql
@@ -0,0 +1,6 @@
+ALTER TABLE doctor
+    ADD COLUMN contact_number VARCHAR(100),
+    ADD COLUMN email VARCHAR(100),
+    ADD COLUMN address VARCHAR(100),
+    ADD COLUMN specialization VARCHAR(100),
+    ADD COLUMN experience_years VARCHAR(100);
