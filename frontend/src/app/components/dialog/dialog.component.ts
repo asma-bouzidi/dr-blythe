2a952e23aa13428530d86949bdbc270d80984313
isra-bouzidi <b.isra2004@gmail.com>
2025-01-23 16:13:34 +0100

    implement list of patients for doctor

--- frontend/src/app/components/dialog/dialog.component.ts
+++ frontend/src/app/components/dialog/dialog.component.ts
@@ -1,21 +1,36 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogContent, MatDialogTitle, MatDialogModule} from '@angular/material/dialog';
 import { DialogData } from '../doctor/doctor-add/doctor-add.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { PatientComponent } from '../patient/patient.component';
 import { PatientService } from 'src/app/services/patient.service';
 import { Patient } from 'src/app/models/patient.model';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { CommonModule } from '@angular/common'; // Import CommonModule for *ngIf and *ngFor
+
 
 @Component({
   selector: 'app-dialog',
   standalone: true,
   imports: [
     MatDialogTitle,
-    MatDialogContent
+    MatTableModule,
+    MatDialogContent,
+    MatDialogModule,
+    MatCheckboxModule,
+    FormsModule,
+    CommonModule
   ],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss'
 })
 export class DialogComponent {
+  patients: Patient[] = [];
+
+  displayedColumns: string[] = ['select', 'name', 'age', 'gender'];
+  message: string = '';
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
     private patientService: PatientService
@@ -23,8 +38,6 @@ export class DialogComponent {
 
   }
 
-
-  patients: Patient[] = [];
 
   ngOnInit(): void {
     this.patientService.getAllPatients().subscribe((data: Patient[]) => {
