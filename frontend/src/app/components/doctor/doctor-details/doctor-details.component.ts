99e687154e07197c3c5ce77230033f13213e3cc8
Isra Bouzidi <b.isra2004@gmail.com>
2025-01-27 22:29:53 +0100

    Feature/DBL-44-refactor-ui (#49)

--- frontend/src/app/components/doctor/doctor-details/doctor-details.component.ts
+++ frontend/src/app/components/doctor/doctor-details/doctor-details.component.ts
@@ -1,15 +1,21 @@
-import {Component, OnInit} from '@angular/core';
-import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
-import {MatIcon} from "@angular/material/icon";
-import {NgIf} from "@angular/common";
-import {MatProgressSpinner} from "@angular/material/progress-spinner";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatSelectModule} from "@angular/material/select";
-import {FormsModule} from "@angular/forms";
-import {NgToastModule} from "ng-angular-popup";
-import {MatTableModule} from "@angular/material/table";
-import {ActivatedRoute} from "@angular/router";
-import {DoctorService} from "../../../services/doctor.service";
+import { Component, OnInit } from '@angular/core';
+import {
+  MatCard,
+  MatCardContent,
+  MatCardHeader,
+  MatCardTitle,
+} from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import { NgIf } from '@angular/common';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { NgToastModule } from 'ng-angular-popup';
+import { MatTableModule } from '@angular/material/table';
+import { ActivatedRoute } from '@angular/router';
+import { DoctorService } from '../../../services/doctor.service';
+import { ScrollerComponent } from '../../scroller/scroller.component';
 
 @Component({
   selector: 'app-doctor-details',
@@ -26,30 +32,27 @@ import {DoctorService} from "../../../services/doctor.service";
     MatCardTitle,
     MatCardHeader,
     MatIcon,
-    MatProgressSpinner
+    MatProgressSpinner,
+    ScrollerComponent,
   ],
   templateUrl: './doctor-details.component.html',
-  styleUrl: './doctor-details.component.scss'
+  styleUrl: './doctor-details.component.scss',
 })
 export class DoctorDetailsComponent implements OnInit {
-
   doctor: any | null = null;
   error: string | null = null;
 
-  displayedColumns: string[] = [
-    'name',
-    'image']
+  displayedColumns: string[] = ['name', 'image'];
 
   message: string = '';
 
   constructor(
     private route: ActivatedRoute,
-    private doctorService: DoctorService,
-  ) {
-  }
+    private doctorService: DoctorService
+  ) {}
 
   ngOnInit(): void {
-    const doctorId = this.route.snapshot.paramMap.get("doctorId");
+    const doctorId = this.route.snapshot.paramMap.get('doctorId');
     if (doctorId) {
       this.loadDoctorData(doctorId);
     } else {
@@ -59,7 +62,7 @@ export class DoctorDetailsComponent implements OnInit {
 
   loadDoctorData(doctorId: string): void {
     this.doctorService.getDoctorById(doctorId).subscribe({
-      next: (data) => (this.doctor = data)
+      next: (data) => (this.doctor = data),
     });
   }
 }
--- frontend/src/app/components/doctor/doctor.component.ts
+++ frontend/src/app/components/doctor/doctor.component.ts
@@ -1,75 +1,82 @@
-import {Component, OnInit} from '@angular/core';
-import {Doctor} from "../../models/doctor.model";
-import {catchError, EMPTY, tap} from "rxjs";
-import {NgToastService} from "ng-angular-popup";
-import {DoctorService} from "../../services/doctor.service";
-import {MatButton, MatIconButton} from "@angular/material/button";
-import {Router} from "@angular/router";
-import {MatTableModule} from '@angular/material/table';
-import {FormsModule} from "@angular/forms";
-import {MatIcon} from "@angular/material/icon";
+import { Component, OnInit } from '@angular/core';
+import { Doctor } from '../../models/doctor.model';
+import { catchError, EMPTY, tap } from 'rxjs';
+import { NgToastService } from 'ng-angular-popup';
+import { DoctorService } from '../../services/doctor.service';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
+import { FormsModule } from '@angular/forms';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-doctor',
   standalone: true,
-  imports: [
-    MatButton,
-    MatTableModule,
-    FormsModule,
-    MatIcon,
-    MatIconButton
-  ],
+  imports: [MatButton, MatTableModule, FormsModule, MatIcon, MatIconButton],
   templateUrl: './doctor.component.html',
-  styleUrl: './doctor.component.scss'
+  styleUrl: './doctor.component.scss',
 })
 export class DoctorComponent implements OnInit {
   doctors: Doctor[] = [];
 
-  displayedColumns: string[] = ['name', 'image', 'actions'];
+  displayedColumns: string[] = [
+    'name',
+    'specialization',
+    'email',
+    'address',
+    'actions',
+  ];
   message: string = '';
 
   constructor(
     private router: Router,
     private doctorService: DoctorService,
     private toast: NgToastService
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
     this.loadDoctors();
   }
 
   loadDoctors(): void {
-    this.doctorService.getAllDoctors().pipe(
-      tap((doctors) => {
-        this.doctors = doctors;
-        this.message = 'Doctors fetched successfully!';
-        this.toast.success(this.message, '', 3000);
-      }),
-      catchError((error) => {
-        this.message = 'Error fetching doctors!';
-        this.toast.danger(this.message, '', 3000);
-        console.error(error);
-        return EMPTY;
-      })
-    ).subscribe();
-  }
-
-  deleteDoctor(doctorId: string): void {
-    if (confirm('Are you sure you want to delete this Doctor?')) {
-      this.doctorService.deleteDoctorById(doctorId).pipe(
-        tap(() => {
-          this.doctors = this.doctors.filter(doctor => doctor.id !== doctorId);
-          this.message = 'Doctor deleted successfully';
+    this.doctorService
+      .getAllDoctors()
+      .pipe(
+        tap((doctors) => {
+          this.doctors = doctors;
+          this.message = 'Doctors fetched successfully!';
           this.toast.success(this.message, '', 3000);
         }),
         catchError((error) => {
-          this.message = 'Error deleting doctor';
+          this.message = 'Error fetching doctors!';
           this.toast.danger(this.message, '', 3000);
           console.error(error);
           return EMPTY;
         })
-      ).subscribe();
+      )
+      .subscribe();
+  }
+
+  deleteDoctor(doctorId: string): void {
+    if (confirm('Are you sure you want to delete this Doctor?')) {
+      this.doctorService
+        .deleteDoctorById(doctorId)
+        .pipe(
+          tap(() => {
+            this.doctors = this.doctors.filter(
+              (doctor) => doctor.id !== doctorId
+            );
+            this.message = 'Doctor deleted successfully';
+            this.toast.success(this.message, '', 3000);
+          }),
+          catchError((error) => {
+            this.message = 'Error deleting doctor';
+            this.toast.danger(this.message, '', 3000);
+            console.error(error);
+            return EMPTY;
+          })
+        )
+        .subscribe();
     }
   }
 
